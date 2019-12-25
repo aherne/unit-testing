485f26064a326ee957af9d213bc5e08cdc42fd98
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-12-25 08:23:12 +0200

    bugfix

--- src/Runner/ClassesFinder.php
+++ src/Runner/ClassesFinder.php
@@ -43,10 +43,7 @@ class ClassesFinder
             }
             $path = $directory."/".$file;
             if (is_dir($path)) {
-                $temp = $this->getFiles($path);
-                foreach ($temp as $fileInfo) {
-                    $this->results[$fileInfo["class"]] = $fileInfo;
-                }
+                $this->setResults($path);
             } elseif (strpos($file, ".php")) {
                 $info = $this->readFile($path);
                 if ($info->className) {
@@ -88,7 +85,7 @@ class ClassesFinder
         $classInfo->extends = (!empty($m2[7])?$this->getFullClassName($m2[7], $correspondences,$classInfo->namespace):"");
         if (!empty($m2[9])) {
             $matches = array();
-            preg_match_all("/([_a-zA-Z0-9\\\]+))/", $m2[9], $matches);
+            preg_match_all("/([_a-zA-Z0-9\\\]+)/", $m2[9], $matches);
             foreach ($matches[1] as $className) {
                 $classInfo->implements[] = $this->getFullClassName($className, $correspondences,$classInfo->namespace);
             }            
@@ -105,7 +102,15 @@ class ClassesFinder
         return $classInfo;
     }
     
-    private function getFullClassName($name, $correspondences, $namespace)
+    /**
+     * Gets full class name
+     * 
+     * @param string $name
+     * @param array $correspondences
+     * @param string $namespace
+     * @return string
+     */
+    private function getFullClassName(string $name, array $correspondences, string $namespace): string
     {
         return (isset($correspondences[$name])?$correspondences[$name]:($name[0]=="\\"?$name:($namespace?$namespace."\\".$name:$name)));
     }
@@ -116,24 +121,49 @@ class ClassesFinder
      * @param ClassInfo $classInfo
      * @return string[]
      */
-    private function getInheritedMethods(ClassInfo $classInfo)
+    private function getInheritedMethods(ClassInfo $classInfo): array
     {
         $methods = $classInfo->methods;
         if ($classInfo->extends) {
-            $newMethods = $this->getInheritedMethods($this->results[$classInfo->extends]);
-            foreach ($newMethods as $methodName) {
-                $methods[$methodName] = $methodName;
-            }
-        }
-        if ($classInfo->implements) {
-            foreach ($classInfo->implements as $className) {
+            $className = $this->getNormalizedClassName($classInfo->extends);
+            if ($className) {
                 $newMethods = $this->getInheritedMethods($this->results[$className]);
                 foreach ($newMethods as $methodName) {
                     $methods[$methodName] = $methodName;
                 }
             }
         }
+        if ($classInfo->implements) {
+            foreach ($classInfo->implements as $className) {
+                $className = $this->getNormalizedClassName($className);
+                if ($className) {
+                    $newMethods = $this->getInheritedMethods($this->results[$className]);
+                    foreach ($newMethods as $methodName) {
+                        $methods[$methodName] = $methodName;
+                    }
+                }
+            }
+        }
         return $methods;
+    }
+    
+    /**
+     * Gets normalized class name, 
+     * 
+     * @param string $className
+     * @return string
+     */
+    private function getNormalizedClassName(string $className): string
+    {
+        if ($className[0]=="\\") {
+            $className = substr($className, 1);    
+        }
+        
+        if (strpos($className, "\\")===false) {
+            return ""; // global namespace class
+        } else {
+            return $className;
+        }
     }
     
     /**
